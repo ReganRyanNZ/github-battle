2ca05780c9da650aaaf679a7512d3998d24e4fa6
Regan Ryan <regan.ryan.nz@gmail.com>
2017-03-21 11:28:06 +1300

    battle flow working, but still dumping JSON for final result

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,43 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
+
+var ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      isLoading: true,
+      playersInfo: [],
+    }
+  },
+  componentDidMount: function () {
+    var query = this.props.location.query;
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
+  },
+  handleInitiateBattle: function () {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
+  render: function () {
+    return (
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
+        playersInfo={this.state.playersInfo} />
+    )
+  }
+});
+
+module.exports = ConfirmBattleContainer;
---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,62 @@
+var axios = require('axios');
+
+// I wont use this, but I might need it if I'm throttled by the api
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var param = "?client_id=" + id + "&client_secret=" + sec;
+
+function getUserInfo(username) {
+  return axios.get('https://api.github.com/users/' + username + param);
+}
+
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars){
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
+var helpers = {
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username)
+    })).then(function(info) {
+      return info.map(function(user) {
+        return user.data;
+      })
+    }).catch(function(err) {
+      console.warn("Error in getPlayersInfo", err);
+    })
+  },
+  battle: function(players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(err){console.warn("Error in getPlayersInfo", err)})
+  }
+};
+
+module.exports = helpers;
